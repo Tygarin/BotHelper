aa27ed421391e7260d63e21487c911f5ead284c4
Danil <dankryto@yandex.ru>
2024-01-11 20:49:32 +0400

    fix: add second admin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 const ADMIN_ID = process.env.ADMIN_ID;
+const SECOND_ADMIN_ID = process.env.SECOND_ADMIN_ID
 
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
@@ -11,6 +12,10 @@ bot.on("chat_join_request", (user) => {
     bot.approveChatJoinRequest(Number(CHANNEL_ID), user.from.id);
     bot.sendMessage(
       Number(ADMIN_ID),
+      `В канал ${user.chat.title} по ссылке ${user.invite_link?.name} зашел пользователь ${user.from.first_name}`
+    );
+    bot.sendMessage(
+      Number(SECOND_ADMIN_ID),
       `В канал ${user.chat.title} по ссылке ${user.invite_link?.name} зашел пользователь ${user.from.first_name}`
     );
   }

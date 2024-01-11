3b3df2a00cbfed14208f2c4a84bdaf8e3383dd48
Danil <dankryto@yandex.ru>
2024-01-11 20:30:37 +0400

    fix

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 const ADMIN_ID = process.env.ADMIN_ID;
 
 const TelegramBot = require("node-telegram-bot-api");
-console.log(BOT_TOKEN);
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
 bot.on("chat_join_request", (user) => {
-  if (user.chat.id === CHANNEL_ID) {
-    bot.approveChatJoinRequest(CHANNEL_ID, user.from.id);
+  if (user.chat.id === Number(CHANNEL_ID)) {
+    bot.approveChatJoinRequest(Number(CHANNEL_ID), user.from.id);
     bot.sendMessage(
-      ADMIN_ID,
+      Number(ADMIN_ID),
       `В канал ${user.chat.title} по ссылке ${user.invite_link?.name} зашел пользователь ${user.from.first_name}`
     );
   }
-  console.log(user);
 });
